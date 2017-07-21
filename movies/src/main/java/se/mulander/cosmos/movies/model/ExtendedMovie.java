ff8962536f837a7347469c580b3404b4783d621f
Marcus Münger <marcus.munger@24money.se>
2017-07-21 11:18:41 +0200

    Add extended data for each movie object

--- movies/src/main/java/se/mulander/cosmos/movies/model/ExtendedMovie.java
+++ movies/src/main/java/se/mulander/cosmos/movies/model/ExtendedMovie.java
@@ -15,45 +15,63 @@ import java.util.List;
 @Table(name = "extended_movie")
 public class ExtendedMovie
 {
-	@Id
-	@Column(name = "movie_id")
-	@JsonIgnore
-	public String movieID;
+    @Id
+    @Column(name = "movie_id")
+    @JsonIgnore
+    public String movieID;
 
-	@OneToOne(fetch = FetchType.EAGER)
-	@PrimaryKeyJoinColumn
-	public Movie parent;
+    @OneToOne(fetch = FetchType.EAGER)
+    @PrimaryKeyJoinColumn
+    public Movie parent;
 
-	@ApiModelProperty(value = "A short synopsis describing what the movie is about")
-	@Column(name = "description")
-	public String description;
-	@ApiModelProperty(value = "A list of actors who starred in the movie")
-	@ElementCollection
-	@CollectionTable(name = "actors", joinColumns = @JoinColumn(name = "movie_id"))
-	@Column(name = "name")
-	public List<String> actors = new ArrayList<>();
-	@ApiModelProperty(value = "A list of people who directed the movie")
-	@ElementCollection
-	@CollectionTable(name = "directors", joinColumns = @JoinColumn(name = "movie_id"))
-	@Column(name = "name")
-	public List<String> directors = new ArrayList<>();
-	@ApiModelProperty(value = "A list of people who wrote the screenplay for the movie")
-	@ElementCollection
-	@CollectionTable(name = "writers", joinColumns = @JoinColumn(name = "movie_id"))
-	@Column(name = "name")
-	public List<String> writers = new ArrayList<>();
-	@ApiModelProperty(name = "poster_url", value = "An absolute link to a poster image")
-	@SerializedName("poster_url")
-	@Column(name = "poster_url")
-	public String posterURL;
+    @ApiModelProperty(value = "A short synopsis describing what the movie is about")
+    @Column(name = "description")
+    public String description;
+    @ApiModelProperty(value = "A list of actors who starred in the movie")
+    @ElementCollection
+    @CollectionTable(name = "actors", joinColumns = @JoinColumn(name = "movie_id"))
+    @Column(name = "name")
+    public List<String> actors = new ArrayList<>();
+    @ApiModelProperty(value = "A list of people who directed the movie")
+    @ElementCollection
+    @CollectionTable(name = "directors", joinColumns = @JoinColumn(name = "movie_id"))
+    @Column(name = "name")
+    public List<String> directors = new ArrayList<>();
+    @ApiModelProperty(value = "A list of people who wrote the screenplay for the movie")
+    @ElementCollection
+    @CollectionTable(name = "writers", joinColumns = @JoinColumn(name = "movie_id"))
+    @Column(name = "name")
+    public List<String> writers = new ArrayList<>();
+    @ApiModelProperty(name = "poster_url", value = "An absolute link to a poster image")
+    @SerializedName("poster_url")
+    @Column(name = "poster_url")
+    public String posterURL;
 
-	public ExtendedMovie()
-	{
-	}
+    public ExtendedMovie()
+    {
+    }
 
-	public ExtendedMovie(String description, String posterURL)
-	{
-		this.description = description;
-		this.posterURL = posterURL;
-	}
+    public ExtendedMovie(String description, String posterURL)
+    {
+        this.description = description;
+        this.posterURL = posterURL;
+    }
+
+    public ExtendedMovie setActors(List<String> actors)
+    {
+        this.actors = actors;
+        return this;
+    }
+
+    public ExtendedMovie setDirectors(List<String> directors)
+    {
+        this.directors = directors;
+        return this;
+    }
+
+    public ExtendedMovie setWriters(List<String> writers)
+    {
+        this.writers = writers;
+        return this;
+    }
 }
--- movies/src/main/java/se/mulander/cosmos/movies/util/Movies.java
+++ movies/src/main/java/se/mulander/cosmos/movies/util/Movies.java
@@ -2,13 +2,12 @@ package se.mulander.cosmos.movies.util;
 
 import se.mulander.cosmos.common.business.HttpRequest;
 import se.mulander.cosmos.common.database.jpa.Database;
-import se.mulander.cosmos.movies.model.GenreList;
-import se.mulander.cosmos.movies.model.Movie;
-import se.mulander.cosmos.movies.model.Rating;
-import se.mulander.cosmos.movies.model.TMDBResponse;
+import se.mulander.cosmos.movies.model.*;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static se.mulander.cosmos.movies.util.Settings.apiKey;
@@ -33,24 +32,52 @@ public class Movies
         urlBuilder.append("&primary_release_year=2017");
         TMDBResponse response = (TMDBResponse) HttpRequest.getRequest(urlBuilder.toString(), TMDBResponse.class).data;
 
-        List result = Arrays.asList(response.results)
-                            .stream()
-                            .map(tmdb -> {
-                                String year = tmdb.release_date.trim().substring(0, 4);
-                                Movie m = new Movie("https://image.tmdb.org/t/p/w185/" + tmdb.poster_path, tmdb.title,
-                                                    year.matches("\\d+") ? Integer.parseInt(year) : -1).addRating(
-                                        new Rating("The Movie Database", tmdb.vote_average, tmdb.vote_count));
-                                tmdb.genre_ids.stream()
-                                              .map(id -> genreList.genres.stream()
-                                                                         .filter(genre -> genre.id == id)
-                                                                         .findFirst()
-                                                                         .get().name)
-                                              .forEach(name -> m.addGenre(name));
-                                return m;
-                            })
-                            .sorted((m1, m2) -> new Double(m2.rating.get(0).rating).compareTo(m1.rating.get(0).rating))
-                            .collect(Collectors.toList());
-        Database.saveObjects(result);
+        List<Movie> result = Arrays.asList(response.results)
+                                   .stream()
+                                   .map(tmdb -> {
+                                       String year = tmdb.release_date.trim().substring(0, 4);
+                                       Movie m = new Movie("https://image.tmdb.org/t/p/w185/" + tmdb.poster_path,
+                                                           tmdb.title, year.matches("\\d+") ? Integer.parseInt(
+                                               year) : -1).addRating(
+                                               new Rating("The Movie Database", tmdb.vote_average, tmdb.vote_count));
+                                       tmdb.genre_ids.stream()
+                                                     .map(id -> genreList.genres.stream()
+                                                                                .filter(genre -> genre.id == id)
+                                                                                .findFirst()
+                                                                                .get().name)
+                                                     .forEach(name -> m.addGenre(name));
+                                       ExtendedMovie exMovie = new ExtendedMovie(tmdb.overview,
+                                                                                 "https://image.tmdb.org/t/p/w1920/" + tmdb.backdrop_path);
+                                       m.setExtended(exMovie);
+                                       return m;
+                                   })
+                                   .sorted((m1, m2) -> new Double(m2.rating.get(0).rating).compareTo(
+                                           m1.rating.get(0).rating))
+                                   .collect(Collectors.toList());
+        result.stream().forEach(m -> {
+            Map<String, Object> param = new HashMap<>();
+            param.put("title", m.title);
+            param.put("year", m.year);
+            try
+            {
+                List dbMovies = Database.getObjects("from Movie WHERE title = :title AND year = :year", param);
+                if (dbMovies.isEmpty())
+                    Database.saveObject(m);
+                else
+                {
+                    Movie oldMovie = (Movie) dbMovies.get(0);
+                    m.setID(oldMovie.internalID);
+                    Database.updateObject(m);
+                }
+            } catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        });
+        result = result.stream().map(m -> {
+            m.extendedMovie = null;
+            return m;
+        }).collect(Collectors.toList());
         return result;
     }
 
