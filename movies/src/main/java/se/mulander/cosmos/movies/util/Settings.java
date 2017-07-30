3ed7a6fe9ffdc5bfeb714e6dd9765022a6f0dcbc
Marcus M <marcusmunger@hotmail.com>
2017-07-30 21:16:56 +0200

    init is now done on it's own thread so that it can complete even when setting isn't started

--- movies/src/main/java/se/mulander/cosmos/movies/util/Settings.java
+++ movies/src/main/java/se/mulander/cosmos/movies/util/Settings.java
@@ -19,67 +19,73 @@ import java.util.function.Consumer;
  */
 public class Settings
 {
-    public static String theMovieDbURL = "https://api.themoviedb.org";
+	public static String theMovieDbURL = "https://api.themoviedb.org";
 
-    public static String apiKey = "6559b0b40f39a093b15b3c4213bdb613";
+	public static String apiKey = "6559b0b40f39a093b15b3c4213bdb613";
 
-    public static String settingsURL = null;
+	public static String settingsURL = null;
 
-    private static Consumer<String> settingsUpdater = (url) -> {
-        try
-        {
+	private static Consumer<String> settingsUpdater = (url) ->
+	{
+		try
+		{
 
-            RequestConfig.Builder requestBuilder = RequestConfig.custom();
-            HttpClientBuilder builder = HttpClientBuilder.create();
-            builder.setDefaultRequestConfig(requestBuilder.build());
-            HttpClient client = builder.build();
+			RequestConfig.Builder requestBuilder = RequestConfig.custom();
+			HttpClientBuilder builder = HttpClientBuilder.create();
+			builder.setDefaultRequestConfig(requestBuilder.build());
+			HttpClient client = builder.build();
 
-            org.apache.http.HttpResponse response = client.execute(new HttpGet(url));
-            if(response.getStatusLine().getStatusCode() == HttpServletResponse.SC_OK)
-            {
-                ObjectMapper mapper = new ObjectMapper();
-                List<Setting> settings = mapper.readValue(response.getEntity().getContent(), mapper.getTypeFactory().constructCollectionType(List.class, Setting.class));
+			org.apache.http.HttpResponse response = client.execute(new HttpGet(url));
+			if(response.getStatusLine().getStatusCode() == HttpServletResponse.SC_OK)
+			{
+				ObjectMapper mapper = new ObjectMapper();
+				List<Setting> settings = mapper.readValue(response.getEntity().getContent(), mapper.getTypeFactory().constructCollectionType(List.class, Setting.class));
 
-                Optional<Setting> movieSetting = settings.stream()
-                                                           .filter(s -> s.name.equals("movies"))
-                                                           .findFirst();
-                if (movieSetting.isPresent())
-                {
-                    for (Setting s : movieSetting.get().children)
-                    {
-                        if (s.name.equals("movie_db_api_key"))
-                            apiKey = s.value;
-                        else if (s.name.equals("movie_db_api_uri"))
-                            theMovieDbURL = s.value;
-                    }
-                }
-                else
-                {
-                    Setting dbURL = new Setting("movie_db_api_uri",
-                                                "(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]",
-                                                theMovieDbURL);
-                    Setting dbKey = new Setting("movie_db_api_key", ".*", apiKey);
-                    Setting movies = new Setting("movies", dbKey, dbURL);
+				Optional<Setting> movieSetting = settings.stream()
+						.filter(s -> s.name.equals("movies"))
+						.findFirst();
+				if(movieSetting.isPresent())
+				{
+					for(Setting s : movieSetting.get().children)
+					{
+						if(s.name.equals("movie_db_api_key"))
+							apiKey = s.value;
+						else if(s.name.equals("movie_db_api_uri"))
+							theMovieDbURL = s.value;
+					}
+				}
+				else
+				{
+					Setting dbURL = new Setting("movie_db_api_uri",
+							"(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]",
+							theMovieDbURL);
+					Setting dbKey = new Setting("movie_db_api_key", ".*", apiKey);
+					Setting movies = new Setting("movies", dbKey, dbURL);
 
-                    HttpRequest.postRequest(settingsURL + "/settings/register", movies, null);
-                }
-            }
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    };
+					HttpRequest.postRequest(settingsURL + "/settings/register", movies, null);
+				}
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	};
 
-    public static void init()
-    {
-        settingsURL = Scanner.find(8080, "/settings/api/discover") + "/settings/api";
-        settingsUpdater.accept(settingsURL + "/settings/structure");
-        new Thread(() -> {
-            while(true)
-            {
-                settingsUpdater.accept(settingsURL + "/settings/structure/poll");
-            }
-        }).start();
-    }
+	public static void init()
+	{
+		new Thread(() ->
+		{
+			while(settingsURL == null)
+			{
+				settingsURL = Scanner.find(8080, "/settings/api/discover");
+			}
+			settingsURL += "/settings/api";
+			settingsUpdater.accept(settingsURL + "/settings/structure");
+			while(true)
+			{
+				settingsUpdater.accept(settingsURL + "/settings/structure/poll");
+			}
+		}).start();
+	}
 }
