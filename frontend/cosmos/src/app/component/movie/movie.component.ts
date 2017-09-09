f018a52628e4c796d12749973e0ae4185fa153e4
Marcus M <marcusmunger@hotmail.com>
2017-09-09 12:48:14 +0200

    rating releasedate on hover for movies

--- frontend/cosmos/src/app/component/movie/movie.component.ts
+++ frontend/cosmos/src/app/component/movie/movie.component.ts
@@ -11,7 +11,30 @@ export class MovieComponent {
     @Input()
     private movie: Movie;
 
+    private hover: boolean = false;
+
     constructor(private movieService: MovieService)
     {
+    }
+
+    public calcRating(): number
+    {
+        let r = 0;
+        let c = 0;
+        this.movie.rating.forEach(rating => {
+            r += rating.rating/rating.max;
+            c++;
+        });
+        var num = (r/c)*10;
+        num /= 2;
+        return Math.round(num);
+    }
+
+    public asArray(num: number): number[]
+    {
+        let result: number[] = [];
+        for(let i = 0; i < num; i++)
+            result.push(i);
+        return result;
     }
 }
--- frontend/cosmos/src/app/model/rating.model.ts
+++ frontend/cosmos/src/app/model/rating.model.ts
@@ -3,4 +3,5 @@ export class Rating
     public provider: string;
     public rating: number;
     public vote_count: number;
+    public max: number;
 }
