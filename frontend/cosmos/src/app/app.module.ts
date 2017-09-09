c416b657eb7cc731b28aacefe2c0c393ffd1bd5b
Marcus M <marcusmunger@hotmail.com>
2017-09-09 09:25:59 +0200

    base routing done

---
+++ frontend/cosmos/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AppComponent } from './component/app/app.component';
+import { NavbarComponent } from './component/navbar/navbar.component';
+import { MovieListComponent } from './component/movieList/movieList.component';
+import { NotFoundComponent } from './component/notFound/notFound.component';
+
+const appRoutes: Routes = [
+  { path: "movies", component: MovieListComponent },
+  { path: '',
+    redirectTo: 'movies',
+    pathMatch: 'full'
+  },
+  { path: '**', component: NotFoundComponent }
+];
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    MovieListComponent,
+    NotFoundComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/cosmos/src/app/component/movieList/movieList.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'movieList',
+  templateUrl: './movieList.component.html',
+  styleUrls: ['./movieList.component.css']
+})
+export class MovieListComponent {
+  private imagePosters: string[] = ["","","","","","","","","","","","","","","",""];
+}
