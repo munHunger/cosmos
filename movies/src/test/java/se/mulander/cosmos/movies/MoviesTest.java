e6ab189d72159d2fb2412f6014544cfe5db0bd1f
Marcus Münger <marcus.munger@cybercom.com>
2017-10-18 07:11:53 +0200

    Start of testing

--- movies/src/test/java/se/mulander/cosmos/movies/MoviesTest.java
+++ movies/src/test/java/se/mulander/cosmos/movies/MoviesTest.java
@@ -7,6 +7,19 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 import se.mulander.cosmos.movies.impl.Movies;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Response;
+
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.beforeEach;
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.describe;
+import static com.mscharhag.oleaster.runner.StaticRunnerSupport.it;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
+import static org.powermock.api.mockito.PowerMockito.*;
+
 /**
  * @author Marcus Münger
  */
@@ -14,4 +27,38 @@ import se.mulander.cosmos.movies.impl.Movies;
 @PowerMockRunnerDelegate(OleasterRunner.class)
 @PrepareForTest({Movies.class})
 public class MoviesTest {
+
+    {
+        Movies underTest = spy(new Movies());
+        describe("MovieTest", () -> {
+            describe("Recomendations", () -> {
+                beforeEach(() -> {
+                    doReturn(null).when(underTest, "getGenres", any(), anyString(), anyString());
+                });
+                it("fetches a list of genres", () -> {
+
+                });
+            });
+        });
+    }
+
+    //TODO: Where to put this so that everyone can access it?
+    private static <T> void mockResponse(Class<T> type, T response) throws Exception
+    {
+        mockStatic(ClientBuilder.class);
+        Client client = mock(Client.class);
+        when(ClientBuilder.class, "newClient").thenReturn(client);
+        WebTarget webTarget = mock(WebTarget.class);
+        when(client.target(anyString())).thenReturn(webTarget);
+        when(webTarget.path(anyString())).thenReturn(webTarget);
+        when(webTarget.queryParam(any(), any())).thenReturn(webTarget);
+        Invocation.Builder invocationBuilder = mock(Invocation.Builder.class);
+        when(webTarget.request()).thenReturn(invocationBuilder);
+        Invocation invocation = mock(Invocation.class);
+        when(invocationBuilder.buildGet()).thenReturn(invocation);
+
+        Response res = mock(Response.class);
+        when(invocation.invoke()).thenReturn(res);
+        when(res.readEntity(type)).thenReturn(response);
+    }
 }
