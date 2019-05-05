765dbd546de7360e9eeb1db63b263e17512aed17
munHunger <marcusmunger@hotmail.com>
2019-05-05 15:54:41 +0200

    moved query resolution to separate package

---
+++ graphqlHelper/src/index.js
@@ -0,0 +1,105 @@
+const fs = require("fs");
+const { request } = require("graphql-request");
+let structure = undefined;
+
+function queryParser(query) {
+  return query
+    .split("\n")
+    .slice(1, -1)
+    .slice(1, -1)
+    .map(line => ({ name: line.match(/\w+/)[0], value: line }));
+}
+function merge(other) {
+  structure = Object.keys(getStructure())
+    .filter(
+      key => Object.keys(other).filter(otherKey => otherKey === key).length > 0
+    )
+    .map(key => {
+      let res = structure[key];
+      Object.keys(other[key])
+        .filter(
+          field =>
+            Object.keys(res).filter(localKey => localKey === field).length === 0
+        )
+        .forEach(otherField => (res[otherField] = other[key][otherField]));
+      return { name: key, value: res };
+    })
+    .reduce((acc, val) => {
+      acc[val.name.toLowerCase()] = val.value;
+      return acc;
+    }, {});
+  return structure;
+}
+
+function toJSON(schema, endpoint) {
+  return schema
+    .split("\n")
+    .map(s => s.trim())
+    .reduce((acc, val) => {
+      if (val === "}" || val.length == 0) return acc;
+      if (val.indexOf("{") > -1) acc.push([val.replace(/\s{/, "")]);
+      else acc[acc.length - 1].push(val);
+      return acc;
+    }, [])
+    .filter(list => list.length > 0 && list[0].indexOf("type") == 0)
+    .map(list => {
+      let name = list[0].replace(/type\s*/, "");
+      let res = { name, def: {} };
+      list
+        .slice(1)
+        .map(field => ({
+          name: field.substring(
+            0,
+            field.indexOf("(") > -1 ? field.indexOf("(") : field.indexOf(":")
+          ),
+          target: endpoint
+        }))
+        .forEach(field => (res.def[field.name] = field.target));
+      return res;
+    })
+    .reduce((acc, val) => {
+      acc[val.name] = val.def;
+      return acc;
+    }, {});
+}
+
+async function resolveOthers(type, query, ids, services) {
+  console.log(getStructure());
+  console.log(queryParser(query));
+  let parameters = queryParser(query)
+    .filter(p => getStructure()[type][p.name] !== "this")
+    .map(p => p.value)
+    .join("\n");
+  if (parameters.length > 0)
+    return request(
+      `http://${services["tmdb"].ip}:${services["tmdb"].port}/graphql`,
+      `
+query{
+${type}(filter: {
+id:{
+  in:[${ids.join(",")}]
+}
+}){
+id
+${parameters}
+}
+}`
+    ).then(data => data.movie);
+  return undefined;
+}
+
+function load() {
+  structure = toJSON(fs.readFileSync("assets/schema.graphql", "utf8"), "this");
+  return structure;
+}
+
+function getStructure() {
+  return structure ? structure : load();
+}
+module.exports = {
+  queryParser,
+  merge,
+  toJSON,
+  getStructure,
+  resolveOthers
+};
--- library/src/server.js
+++ library/src/server.js
@@ -5,12 +5,13 @@ var express = require("express");
 var graphqlHTTP = require("express-graphql");
 var { buildSchema } = require("graphql");
 const { request } = require("graphql-request");
+const graphqlHelper = require("graphql-helper");
 
 const port = 3342;
 const sdClient = require("sd").start("library", port);
 
-let tmdb = undefined;
-sdClient.waitFor("tmdb", config => (tmdb = config));
+let services = undefined;
+sdClient.waitFor("tmdb", config => (services = { tmdb: config }));
 
 let data = [
   {
@@ -18,41 +19,22 @@ let data = [
     status: "WISHLIST"
   }
 ];
-function queryParser(query) {
-  console.log(query);
-  return query
-    .split("\n")
-    .slice(1, -1)
-    .slice(1, -1)
-    .map(line => ({ name: line.match(/\w+/)[0], value: line }));
-}
+
 const server = (req, param) => {
   return {
     movie: async () => {
-      let structure = merge(
-        toJSON(fs.readFileSync("assets/schema.graphql", "utf8"), "this"),
-        toJSON(fs.readFileSync("assets/other.graphql", "utf8"), "tmdb")
+      let structure = graphqlHelper.merge(
+        graphqlHelper.toJSON(
+          fs.readFileSync("assets/other.graphql", "utf8"),
+          "tmdb"
+        )
       ).movie;
-      let parameters = queryParser(param.query)
-        .filter(p => structure[p.name] !== "this")
-        .map(p => p.value)
-        .join("\n");
-      let res = undefined;
-      if (parameters.length > 0)
-        res = await request(
-          `http://${tmdb.ip}:${tmdb.port}/graphql`,
-          `
-query{
-  movie(filter: {
-    id:{
-      in:[${data.map(d => d.id).join(",")}]
-    }
-  }){
-    id
-    ${parameters}
-  }
-}`
-        ).then(data => data.movie);
+      let res = await graphqlHelper.resolveOthers(
+        "movie",
+        param.query,
+        data.map(d => d.id),
+        services
+      );
       return data.map(data =>
         Object.keys(structure)
           .map(key => ({
@@ -79,67 +61,6 @@ query{
   };
 };
 
-console.log(toJSON(fs.readFileSync("assets/schema.graphql", "utf8"), "this"));
-console.log(
-  merge(
-    toJSON(fs.readFileSync("assets/schema.graphql", "utf8"), "this"),
-    toJSON(fs.readFileSync("assets/other.graphql", "utf8"), "tmdb")
-  )
-);
-
-function merge(local, other) {
-  return Object.keys(local)
-    .filter(
-      key => Object.keys(other).filter(otherKey => otherKey === key).length > 0
-    )
-    .map(key => {
-      let res = local[key];
-      Object.keys(other[key])
-        .filter(
-          field =>
-            Object.keys(res).filter(localKey => localKey === field).length === 0
-        )
-        .forEach(otherField => (res[otherField] = other[key][otherField]));
-      return { name: key, value: res };
-    })
-    .reduce((acc, val) => {
-      acc[val.name.toLowerCase()] = val.value;
-      return acc;
-    }, {});
-}
-
-function toJSON(schema, endpoint) {
-  return schema
-    .split("\n")
-    .map(s => s.trim())
-    .reduce((acc, val) => {
-      if (val === "}" || val.length == 0) return acc;
-      if (val.indexOf("{") > -1) acc.push([val.replace(/\s{/, "")]);
-      else acc[acc.length - 1].push(val);
-      return acc;
-    }, [])
-    .filter(list => list.length > 0 && list[0].indexOf("type") == 0)
-    .map(list => {
-      let name = list[0].replace(/type\s*/, "");
-      let res = { name, def: {} };
-      list
-        .slice(1)
-        .map(field => ({
-          name: field.substring(
-            0,
-            field.indexOf("(") > -1 ? field.indexOf("(") : field.indexOf(":")
-          ),
-          target: endpoint
-        }))
-        .forEach(field => (res.def[field.name] = field.target));
-      return res;
-    })
-    .reduce((acc, val) => {
-      acc[val.name] = val.def;
-      return acc;
-    }, {});
-}
-
 startServer(port);
 
 function startServer(port) {
@@ -157,8 +78,6 @@ function startServer(port) {
 }
 
 function loadSchema() {
-  let res = buildSchema(fs.readFileSync("assets/schema.graphql", "utf8"));
-  let other = buildSchema(fs.readFileSync("assets/other.graphql", "utf8"));
   return buildSchema(
     mergeTypes(
       [
