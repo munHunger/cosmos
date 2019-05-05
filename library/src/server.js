9c43d23189a9ad06fc6b000920d4e06f37c6c978
munHunger <marcusmunger@hotmail.com>
2019-05-05 13:27:26 +0200

    Ugly, but can cross serve data

--- library/src/server.js
+++ library/src/server.js
@@ -1,3 +1,5 @@
+var { mergeTypes } = require("merge-graphql-schemas");
+
 const fs = require("fs");
 var express = require("express");
 var graphqlHTTP = require("express-graphql");
@@ -10,10 +12,20 @@ const sdClient = require("sd").start("library", port);
 let tmdb = undefined;
 sdClient.waitFor("tmdb", config => (tmdb = config));
 
-let data = [];
-const server = () => {
+let data = [
+  {
+    id: 299534,
+    status: "WISHLIST"
+  }
+];
+const server = (req, param) => {
+  console.log(param);
   return {
-    movies: async () => {
+    movie: async () => {
+      let structure = merge(
+        toJSON(fs.readFileSync("assets/schema.graphql", "utf8"), "this"),
+        toJSON(fs.readFileSync("assets/other.graphql", "utf8"), "tmdb")
+      ).movie;
       let res = await request(
         `http://${tmdb.ip}:${tmdb.port}/graphql`,
         `
@@ -35,8 +47,20 @@ query{
   }
 }`
       ).then(data => data.movie);
-      console.log(res);
-      return res;
+      return data.map(data =>
+        Object.keys(structure)
+          .map(key => ({
+            name: key,
+            value: input => {
+              if (structure[key] === "this") return data[key];
+              else return res.find(r => r.id === data.id)[key];
+            }
+          }))
+          .reduce((acc, val) => {
+            acc[val.name] = val.value;
+            return acc;
+          }, {})
+      );
     },
     addToWishlist: input => {
       data.push({
@@ -47,22 +71,93 @@ query{
   };
 };
 
+console.log(toJSON(fs.readFileSync("assets/schema.graphql", "utf8"), "this"));
+console.log(
+  merge(
+    toJSON(fs.readFileSync("assets/schema.graphql", "utf8"), "this"),
+    toJSON(fs.readFileSync("assets/other.graphql", "utf8"), "tmdb")
+  )
+);
+
+function merge(local, other) {
+  return Object.keys(local)
+    .filter(
+      key => Object.keys(other).filter(otherKey => otherKey === key).length > 0
+    )
+    .map(key => {
+      let res = local[key];
+      Object.keys(other[key])
+        .filter(
+          field =>
+            Object.keys(res).filter(localKey => localKey === field).length === 0
+        )
+        .forEach(otherField => (res[otherField] = other[key][otherField]));
+      return { name: key, value: res };
+    })
+    .reduce((acc, val) => {
+      acc[val.name.toLowerCase()] = val.value;
+      return acc;
+    }, {});
+}
+
+function toJSON(schema, endpoint) {
+  return schema
+    .split("\n")
+    .map(s => s.trim())
+    .reduce((acc, val) => {
+      if (val === "}" || val.length == 0) return acc;
+      if (val.indexOf("{") > -1) acc.push([val.replace(/\s{/, "")]);
+      else acc[acc.length - 1].push(val);
+      return acc;
+    }, [])
+    .filter(list => list.length > 0 && list[0].indexOf("type") == 0)
+    .map(list => {
+      let name = list[0].replace(/type\s*/, "");
+      let res = { name, def: {} };
+      list
+        .slice(1)
+        .map(field => ({
+          name: field.substring(
+            0,
+            field.indexOf("(") > -1 ? field.indexOf("(") : field.indexOf(":")
+          ),
+          target: endpoint
+        }))
+        .forEach(field => (res.def[field.name] = field.target));
+      return res;
+    })
+    .reduce((acc, val) => {
+      acc[val.name] = val.def;
+      return acc;
+    }, {});
+}
+
 startServer(port);
 
 function startServer(port) {
   var app = express();
   app.use(
     "/graphql",
-    graphqlHTTP({
+    graphqlHTTP(async (req, res, graphQLParams) => ({
       schema: loadSchema(),
-      rootValue: server(),
+      rootValue: await server(req, graphQLParams),
       graphiql: true
-    })
+    }))
   );
   app.listen(port);
   console.log(`Library server up and running on localhost:${port}/graphql`);
 }
 
 function loadSchema() {
-  return buildSchema(fs.readFileSync("assets/schema.graphql", "utf8"));
+  let res = buildSchema(fs.readFileSync("assets/schema.graphql", "utf8"));
+  let other = buildSchema(fs.readFileSync("assets/other.graphql", "utf8"));
+  return buildSchema(
+    mergeTypes(
+      [
+        fs.readFileSync("assets/schema.graphql", "utf8"),
+        fs.readFileSync("assets/other.graphql", "utf8")
+      ],
+      { all: true }
+    )
+  );
 }
